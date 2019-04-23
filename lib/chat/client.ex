426ab0235b98ba3efa154cce345bc154170fbe7b
Kevin Baird <kevincbaird@gmail.com>
2019-04-23 11:07:40 -0700

    Client can now connect

--- lib/chat/client.ex
+++ lib/chat/client.ex
@@ -1,13 +1,14 @@
 defmodule Chat.Client do
   require Logger
 
-  @host "localhost"
+  @host 'localhost'
+  @opts [:binary, active: false]
 
   def run() do
     port =
       String.to_integer(System.get_env("PORT") || raise("missing $PORT environment variable"))
     Logger.info("I have started the client")
-    {:ok, socket} = :gen_tcp.connect(@host, port, [{:active, true}])
+    {:ok, socket} = :gen_tcp.connect(@host, port, @opts)
     run(socket)
   end
 
