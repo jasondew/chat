d6d2f1792870a67e3bfa8f782fbe32917488c0a1
Kevin Baird <kevincbaird@gmail.com>
2019-04-24 16:36:25 -0700

    Agree with session commands

--- lib/chat/client.ex
+++ lib/chat/client.ex
@@ -5,11 +5,11 @@ defmodule Chat.Client do
   @opts [:binary, active: false]
 
   @recognized_commands """
-?       Shows this list
-jr foo  Join the room with the name 'foo'
-lr      Lists the current rooms
-nr foo  Creates a new room with the name 'foo'
-q       Quits the client
+/?          Shows this list
+/create foo Creates a new room with the name 'foo'
+/join foo   Join the room with the name 'foo'
+/list_rooms Lists the current rooms
+/q          Quits the client
 """
 
   def run() do
@@ -21,30 +21,30 @@ q       Quits the client
 
   ### PRIVATE FUNCTIONS
 
-  defp execute_command("?", socket) do
+  defp execute_command("/?", socket) do
     IO.puts(@recognized_commands)
     receive_command(socket)
   end
 
-  defp execute_command("jr " <> room_name, socket) do
+  defp execute_command("/join " <> room_name, socket) do
     IO.puts("TODO: join room with name '#{room_name}'")
-    write_line(socket, "join room " <> room_name)
+    write_line(socket, "/join " <> room_name)
     receive_command(socket)
   end
 
-  defp execute_command("lr", socket) do
+  defp execute_command("/list_rooms", socket) do
     IO.puts("TODO: list rooms")
-    write_line(socket, "list rooms")
+    write_line(socket, "/list_rooms")
     receive_command(socket)
   end
 
-  defp execute_command("nr " <> room_name, socket) do
+  defp execute_command("/create " <> room_name, socket) do
     IO.puts("TODO: new room with name '#{room_name}'")
-    write_line(socket, "new room " <> room_name)
+    write_line(socket, "/create " <> room_name)
     receive_command(socket)
   end
 
-  defp execute_command("q", _socket) do
+  defp execute_command("/q", _socket) do
     System.halt()
   end
 
@@ -54,7 +54,7 @@ q       Quits the client
   end
 
   defp receive_command(socket) do
-    "\nPlease provide a command. ? for options> "
+    "\nPlease provide a command. /? for options> "
     |> IO.gets()
     |> String.trim()
     |> String.downcase()
